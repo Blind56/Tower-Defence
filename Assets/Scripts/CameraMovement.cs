5e52a8758b55e5c476636f8783554bede60ccadd
Blind56 <petrinko6@gmail.com>
2015-06-27 15:33:34 +0300

    3:33 апдейт
    + зум камери

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,19 +5,42 @@ public class CameraMovement : MonoBehaviour {
 	
 	[Range(0, 1f)]
 	public float cameraSensitivity = 0.2f;
+	public float orthoZoomSpeed = .5f;
+	public float perspectiveZoomSpeed = .5f;
+	public float maxOrthoSize = 20;
+	public float minOrthoSize = 5;
+	public float maxFOV = 60;
+	public float minFOV = 20;
 
 	private bool inputType;
 	private float x, z;
 
-	// Use this for initialization
-	void Start () {
-	
+	void Update () {
+		if (Input.touchCount == 2) {
+			Touch touchZero = Input.GetTouch (0);
+			Touch touchOne = Input.GetTouch (1);
+			Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+			Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+			float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+			float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+			float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+			if (Camera.main.orthographic) {
+				Camera.main.orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
+				Camera.main.orthographicSize = Mathf.Clamp (Camera.main.orthographicSize, minOrthoSize, maxOrthoSize);
+			} else {
+				Camera.main.fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
+				Camera.main.fieldOfView = Mathf.Clamp (Camera.main.fieldOfView, minFOV, maxFOV);
+			}
+		}
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+	void FixedUpdate () 
+	{
 		if (Input.touchSupported) {
-			if (Input.touchCount > 0) {
+			if (Input.touchCount == 1) {
 				Touch touch = Input.touches[0];
 				inputType = touch.phase == TouchPhase.Moved;
 				x = touch.deltaPosition.x * (cameraSensitivity / (Screen.currentResolution.height / 100));
